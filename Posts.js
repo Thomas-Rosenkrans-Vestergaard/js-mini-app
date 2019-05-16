9399bf94d0d2309506a583cbb66bee17aa49758a
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2019-05-16 13:24:59 +0200

    view post images

--- Posts.js
+++ Posts.js
@@ -4,7 +4,8 @@ import { gql } from "apollo-boost";
 import { Query, ApolloConsumer } from 'react-apollo';
 import { View, Text } from 'react-native';
 import Markdown from 'react-native-markdown-renderer';
-import { ScrollView } from 'react-native';
+import { ScrollView, BackHandler } from 'react-native';
+import Gallery from 'react-native-image-gallery';
 
 const POSTS = gql`
 {
@@ -13,9 +14,8 @@ const POSTS = gql`
         title
         content
         created
-        likedBy {
-            identifier
-        }
+        likedByCount
+        imagesCount
         position {
           longitude
           latitude
@@ -29,36 +29,71 @@ const POSTS = gql`
 }
 `;
 
+const GET_IMAGES = gql`
+query getPostImages($post: ID!) {
+    getPostImages(post: $post)
+}
+`;
+
 export default class Posts extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        galleryOpen: false,
+        galleryImages: []
     }
 
     render() {
 
         return (
-            <ScrollView style={{ padding: 20, paddingTop: 40 }}>
-                <View marginBottom={40}>
-                    <Query query={POSTS}>
-                        {({ loading, error, data, refetch }) => {
-                            if (loading)
-                                return <Text>The posts are loading.</Text>
-                            if (error)
-                                return <Text>Could not load posts.</Text>
+            <ApolloConsumer>{client => {
+                if (this.state.galleryOpen)
+                    return (<Gallery
+                        style={{ flex: 1, backgroundColor: 'black' }}
+                        images={this.state.galleryImages.map(image => ({
+                            source: { uri: image }
+                        }))}
+                    />)
 
-                            return data.getPosts.map(post => <View key={post.identifier}>
-                                <Text style={{ fontSize: 20, marginBottom: 10 }}>{post.title}</Text>
-                                <Text style={{ fontWeight: 'bold', marginBottom: 5 }}>{post.author.firstName} {post.author.lastName}</Text>
-                                <Text style={{ fontWeight: 'bold', marginBottom: 5 }}>{post.created}</Text>
-                                <Text style={{ fontWeight: 'bold', marginBottom: 5 }}>{post.likedBy.length} likes</Text>
-                                <View marginBottom={40}>
-                                    <Markdown>{post.content.replace('<br>', '')}</Markdown>
-                                </View>
-                            </View>);
-                        }}
-                    </Query>
-                </View>
-            </ScrollView>)
+                return <ScrollView style={{ padding: 20, paddingTop: 40 }}>
+                    <View marginBottom={40}>
+                        <Query query={POSTS}>
+                            {({ loading, error, data, refetch }) => {
+                                if (loading)
+                                    return <Text>The posts are loading.</Text>
+                                if (error)
+                                    return <Text>Could not load posts.</Text>
+
+                                return data.getPosts.map(post => <View key={post.identifier}>
+                                    <Text style={{ fontSize: 20, marginBottom: 10 }}>{post.title}</Text>
+                                    <Text style={{ fontWeight: 'bold', marginBottom: 5 }}>{post.author.firstName} {post.author.lastName}</Text>
+                                    <Text style={{ fontWeight: 'bold', marginBottom: 5 }}>{post.created}</Text>
+                                    <Text style={{ fontWeight: 'bold', marginBottom: 5 }}>{post.likedByCount} likes</Text>
+                                    {post.imagesCount && <Text style={{ color: '#728bd4' }} onPress={() => this.viewImages(client, post.identifier)}>View {post.imagesCount} image(s)</Text>}
+                                    <View marginBottom={40}>
+                                        <Markdown>{post.content.replace('<br>', '')}</Markdown>
+                                    </View>
+                                </View>);
+                            }}
+                        </Query>
+                    </View>
+                </ScrollView>
+            }}
+            </ApolloConsumer>)
     }
+
+    viewImages = async (client, postId) => {
+        BackHandler.addEventListener('hardwareBackPress', this.onBackButtonPress)  
+        const {data} = await client.query({query: GET_IMAGES, variables: {post: postId}});
+        this.setState({ galleryOpen: true, galleryImages: data.getPostImages })
+    }
+
+    onBackButtonPress = () => {
+        if (this.state.galleryOpen) {
+            this.setState({ galleryOpen: false, galleryImages: [] })
+            return true;
+        } else {
+            return false;
+        }
+    };
 }
